74b887614b8271495a77ddb1a3dc8751151ef68e
luciima <luciiamar19@gmail.com>
2023-01-16 12:51:40 -0300

    Agrega validaciones de las funciones que faltan
    O sea, las funciones que están en clase-6-calculos.js

--- tarea/clase-8/clase-8-pruebas.js
+++ tarea/clase-8/clase-8-pruebas.js
@@ -19,6 +19,7 @@ function probarValidarEdad() {
         validarEdad("13124124") === "Los campos edad deben tener entre 1 y 3 números.",
         "validarEdad no rechazó una edad muy larga."
     );
+    console.assert(validarEdad("21") === "", "validarEdad rechazó una edad válida.");
 }
 
 function probarValidarCantidadIntegrantes() {
@@ -42,7 +43,47 @@ function probarValidarCantidadIntegrantes() {
             "El campo de cantidad de integrantes debe tener un número entero.",
         "validarCantidadIntegrantes no funcionó con un número con decimales."
     );
+    console.assert(
+        validarCantidadIntegrantes("3") === "",
+        "validarCantidadIntegrantes rechazó una cantidad de integrantes válida."
+    );
+}
+
+function probarHallarMayorNumero() {
+    console.assert(
+        hallarMayorNumero([1, 2, 3, 4, 5, 23, 42, 12.21, 15]) === 42,
+        "hallarMayorNumero no devolvió el mayor número."
+    );
+    console.assert(
+        hallarMayorNumero([-23, -21, 24, -756, 121]) === 121,
+        "hallarMayorNumero no devolvió el mayor número."
+    );
+}
+
+function probarHallarMenorNumero() {
+    console.assert(
+        hallarMenorNumero([2, 1, 3, 4, 5, 23, 42, 12.21, 15]) === 1,
+        "hallarMenorNumero no devolvió el menor número."
+    );
+    console.assert(
+        hallarMenorNumero([-23, -21, 24, -756, 121]) === -756,
+        "hallarMenorNumero no devolvió el menor número."
+    );
+}
+
+function probarHallarPromedio() {
+    console.assert(
+        hallarPromedio([2, 1, 3, 4, 5, 23, 42, 12, 15]) === 11.88888888888889,
+        "hallarPromedio no devolvió el promedio correcto."
+    );
+    console.assert(
+        hallarPromedio([-23, -21, 24, -756, 121]) === -131,
+        "hallarPromedio no devolvió el promedio correcto."
+    );
 }
 
 probarValidarCantidadIntegrantes();
 probarValidarEdad();
+probarHallarMayorNumero();
+probarHallarMenorNumero();
+probarHallarPromedio();
